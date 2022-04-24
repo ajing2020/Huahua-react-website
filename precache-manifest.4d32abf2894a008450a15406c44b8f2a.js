2ea0936cf2455e8371bac0f3c34f8ae3e932e946
ajing2020 <1277483876@qq.com>
2022-04-24 14:37:49 +0800

    init

---
+++ precache-manifest.4d32abf2894a008450a15406c44b8f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecff9d587d881e046a4b8ceef63bd01",
+    "url": "/Huahua-react-website/index.html"
+  },
+  {
+    "revision": "77186c971d65211791e5",
+    "url": "/Huahua-react-website/static/css/main.744dd75a.chunk.css"
+  },
+  {
+    "revision": "b4a8361e6df5779e324a",
+    "url": "/Huahua-react-website/static/js/2.9517e0cd.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/Huahua-react-website/static/js/2.9517e0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77186c971d65211791e5",
+    "url": "/Huahua-react-website/static/js/main.2dbd4ddf.chunk.js"
+  },
+  {
+    "revision": "424a9d0cb75a80f13b15",
+    "url": "/Huahua-react-website/static/js/runtime-main.ebf0e7ed.js"
+  }
+]);
